d01e98bd6e61b938a13f75203a7069a9ac1d9d01
Bgeri8791 <buzasi.geri@gmail.com>
2022-11-22 20:50:02 +0100

    feladat leírás

--- 01.py
+++ 01.py
@@ -5,3 +5,16 @@ Koch Nikolett
 Budai Ákos
 Buzási Gergő
 """
+#Mozi, 3 terem, napi 3 előadás.
+#Terem: 10sor 10szék
+#Mozijegy:2000Ft
+#alapfeladat: 1 nap foglalás le tudjunk kezelni. 
+# Bárhová tudjunk foglalni. 
+# Lekérdezés mennyi a napi bevétel + aktuális napi bevétel. 
+# kiiratás aktuális tartalom.
+#file törölni is lehessen.
+#pl: Tiszta lappal inditasz? vagy előzőt tölse be?
+#függvények+eljárások
+#Extra: 1 hét lekérdezés
+#szürés:szűrés lehessen betenni.
+#több file-al dolgozni
