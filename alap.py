66971146aa111e2f9697dea981ce7a84bb45d80b
Bgeri8791 <buzasi.geri@gmail.com>
2022-11-23 20:45:46 +0100

    geri02

--- alap.py
+++ alap.py
@@ -1,5 +1,16 @@
 import random
+print("Betöltse az előző lekérdezést?\nIgen-->1\nNem-->2\n")
+valasz= int(input("Választás: "))
+
+if(valasz == 1):
+    with open("export.txt", 'r', encoding='utf-8') as beofile:
+        for adat in beofile:
+            adat.split(":")
+            napibevetel ={int(adat[0])}
+             
+
 napibevetel = 0
+"""
 for i in range(3):
     sor = 1
     while sor <= 10:
@@ -13,6 +24,6 @@ for i in range(3):
         print("")
         sor= sor+1
     print("-------------------------------")
-
-print(f"A 3 terem mai bevétel: {napibevetel}Ft")
+"""
+print(f"Napi bevétel: {napibevetel}Ft")
 
