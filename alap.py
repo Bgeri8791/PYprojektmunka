cd35e094eb36e0b5e148b508f92d5745901706e4
Bgeri8791 <buzasi.geri@gmail.com>
2022-11-23 21:05:30 +0100

    Előzőbevétel betöltés

--- alap.py
+++ alap.py
@@ -1,16 +1,49 @@
 import random
 print("Betöltse az előző lekérdezést?\nIgen-->1\nNem-->2\n")
 valasz= int(input("Választás: "))
+napibevetel = 0
 
 if(valasz == 1):
     with open("export.txt", 'r', encoding='utf-8') as beofile:
         for adat in beofile:
-            adat.split(":")
-            napibevetel ={int(adat[0])}
-             
+            adat.strip()
+            napibevetel = int(adat)
+            print(napibevetel)
+    for i in range(3):
+        sor = 1
+        while sor <= 10:
+            oszlop = 1
+            while oszlop <=10:
+                randomszam = random.randint(0, 1)
+                print(randomszam, end=" ")
+                if(randomszam == 1):
+                    napibevetel += 2500
+                oszlop=oszlop+1
+            print("")
+            sor= sor+1
+    print("-------------------------------")
+    print(f"Az előző és aznapi bevétel összesen:{napibevetel}Ft")
+elif(valasz == 2):
+    for i in range(3):
+        sor = 1
+        while sor <= 10:
+            oszlop = 1
+            while oszlop <=10:
+                randomszam = random.randint(0, 1)
+                print(randomszam, end=" ")
+                if(randomszam == 1):
+                    napibevetel += 2500
+                oszlop=oszlop+1
+            print("")
+            sor= sor+1
+    print("-------------------------------")
+    print(f"Napi bevétel:{napibevetel}Ft")
+        
+          
 
-napibevetel = 0
+
 """
+#Mentés
 for i in range(3):
     sor = 1
     while sor <= 10:
@@ -24,6 +57,8 @@ for i in range(3):
         print("")
         sor= sor+1
     print("-------------------------------")
-"""
-print(f"Napi bevétel: {napibevetel}Ft")
+
+
+print(f"Napi bevétel:{napibevetel}Ft")
+"""  
 
