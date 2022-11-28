26d16ee594ca176d689fff7cdd2524c8a5395e7c
Bgeri8791 <buzasi.geri@gmail.com>
2022-11-28 12:17:48 +0100

    Szuroletrehozas

--- alap.py
+++ alap.py
@@ -1,6 +1,5 @@
-import random
-
-def Mozi(teremdb,napibevetel):
+import random 
+def NapMozi(teremdb,napibevetel):
     for i in range(3):
         #Itt írja ki a hányas terem aztán nőveli 1-el. Csak 3-ig megy el.
         print(f"Terem_{teremdb}")
@@ -12,9 +11,32 @@ def Mozi(teremdb,napibevetel):
         while sor <= 10:
             oszlop = 1
             while oszlop <=10:
-                rszam = random.randint(0, 1)
+                rszam = random.randint(0,1)
                 print(rszam, end=" ")
-                if(rszam == 1): # Ha 1-es a random szám akkor a napibevételhez adjon 2500Ft-ot. (Ha 0 akkor nem csinál semmit.)
+                if(rszam == 1): # Ha 1-es a random szám akkor a napibevételhez adjon 2000Ft-ot. (Ha 0 akkor nem csinál semmit.)
+                    napibevetel += 2000
+                oszlop=oszlop+1
+            print("")
+            sor= sor+1
+        print("-------------------------------") #Elválasztó sor.
+    print(f"A {valasznap}-i bevétel: {napibevetel} Ft\n") 
+    with open("napiexport.txt","w",encoding="utf-8") as kiirexport:
+        kiirexport.write(f"A {valasznap}-i bevétel: {napibevetel} Ft\n")
+def MaiMozi(teremdb,napibevetel):
+    for i in range(3):
+        #Itt írja ki a hányas terem aztán nőveli 1-el. Csak 3-ig megy el.
+        print(f"Terem_{teremdb}")
+        teremdb+=1
+        #Random 0-1 között írja ki a számokat 10x10 oszlop és sorban.
+        #Ha 1-es akkor azon a széken van foglalás.
+        #Ha 2-es akkor szabad a szék.
+        sor = 1
+        while sor <= 10:
+            oszlop = 1
+            while oszlop <=10:
+                rszam = random.randint(0,1)
+                print(rszam, end=" ")
+                if(rszam == 1): # Ha 1-es a random szám akkor a napibevételhez adjon 2000Ft-ot. (Ha 0 akkor nem csinál semmit.)
                     napibevetel += 2000
                 oszlop=oszlop+1
             print("")
@@ -26,50 +48,34 @@ def Mozi(teremdb,napibevetel):
 #Úgyan van kiszámolva hogy az összeg hányadik helyen van.
         kiirexport.write(f"A mai napi bevétel: {napibevetel} Ft\n")
 
-#Menü rendszer 01:   
-#Az előző napi bevételt betöltse-e?
-print("Betöltse az előző lekérdezést?\nIgen-->1\nNem-->2\n")
-valasz= int(input("Választás: "))
-print("")
+
 
 #Alap változók.
 napibevetel = 0
 teremdb=1
-
-#Menü rendszer 02: 
-#1-> szeretném az előző export.txt file betölteni. 
-#2-> Nem szeretném az előző export.txt file betölteni.
-if(valasz == 1):
-    with open("export.txt", 'r', encoding='utf-8') as beofile:
-        for adat in beofile:
-            adatok = adat.strip().split(" ")
-            #szoveg=int(adatok[4])
-            napibevetel = int(adatok[4])
-            print(f"Az előző napi bevétel:{napibevetel}")
-    Mozi(teremdb,napibevetel)
-elif(valasz == 2):
-    Mozi(teremdb,napibevetel)
+Szuro=["Hétfő","Kedd","Szerda","Csütörtök","Péntek","Szombat","Vasárnap"]
 
 
-"""
-#Random 0-1 között írja ki a számokat 10x10-es oszlop és sorban.
-for i in range(3):
-        print(f"Terem_{teremdb}")
-        teremdb+=1
-        sor = 1
-        while sor <= 10:
-            oszlop = 1
-            while oszlop <=10:
-                rszam = random.randint(0, 1)
-                print(rszam, end=" ")
-                if(rszam == 1):
-                    napibevetel += 2500
-                oszlop=oszlop+1
-            print("")
-            sor= sor+1
-        print("-------------------------------")
+
+#Melyik napot tölse be?
+print("Kérlek adj meg a hét napjaiból egyet.\nHa csak a mai napot szeretnéd írd be, hogy 'Ma'")
+valasznap= input("Választott nap: ").capitalize()
 
 
-print(f"Napi bevétel:{napibevetel}Ft")
-"""  
+if(valasznap =="Ma"):
+    print("Betöltse az előző lekérdezést?\n")
+    valasz2= input("Betöltse? (Igen vagy nem): ").capitalize()
+    print("")
+    if(valasz2 == "Igen"):
+        with open("export.txt", 'r', encoding='utf-8') as beofile:
+            for adat in beofile:
+                adatok = adat.strip().split(" ")
+                napibevetel = int(adatok[4])                        
+        print(f"Az előző napi bevétel:{napibevetel}") 
+        MaiMozi(teremdb,napibevetel)
+    elif(valasz2 == "Nem"):
+        MaiMozi(teremdb,napibevetel)
+for i in Szuro:
+    if(valasznap == i):
+        NapMozi(teremdb,napibevetel) 
 
